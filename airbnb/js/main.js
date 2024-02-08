754ced7d42d26b576dd78a94ed7e055ace998c4f
Omar Camara <117489682+Username273183@users.noreply.github.com>
2024-02-08 18:19:59 -0500

    feb8

--- airbnb/js/main.js
+++ airbnb/js/main.js
@@ -29,53 +29,65 @@ closeBtn.addEventListener('click', function () {
 //   description: 'A beautiful room with a king size bed and a view of the city.',
 // };
 
-const rooms = [
-  {
-    name: 'Luxury King Room',
-    price: 300,
-    guests: 2,
-    description:
-      'A beautiful room with a king size bed and a view of the city.',
-  },
-  {
-    name: 'Luxury King Room',
-    price: 300,
-    guests: 2,
-    description:
-      'A beautiful room with a king size bed and a view of the city.',
-  },
-  {
-    name: 'Luxury King Room',
-    price: 300,
-    guests: 2,
-    description:
-      'A beautiful room with a king size bed and a view of the city.',
-  },
-]; // end of rooms
+// const rooms = [
+//   {
+//     name: 'Luxury King Room',
+//     price: 300,
+//     guests: 2,
+//     description:
+//       'A beautiful room with a king size bed and a view of the city.',
+//   },
+//   {
+//     name: 'Luxury King Room',
+//     price: 300,
+//     guests: 2,
+//     description:
+//       'A beautiful room with a king size bed and a view of the city.',
+//   },
+//   {
+//     name: 'Luxury King Room',
+//     price: 300,
+//     guests: 2,
+//     description:
+//       'A beautiful room with a king size bed and a view of the city.',
+//   },
+// ]; // end of rooms
 
-rooms.forEach((room) => {
-  // create elements
-  const roomArticle = document.createElement('article');
-  roomArticle.classList.add('room');
+function renderProperties(properties) {
+  properties.forEach((room) => {
+    // create elements
+    const roomArticle = document.createElement('article');
+    roomArticle.classList.add('room');
 
-  const roomNameElement = document.createElement('h3');
-  roomNameElement.classList.add('room--name');
-  roomNameElement.textContent = room.name;
+    const roomNameElement = document.createElement('h3');
+    roomNameElement.classList.add('room--name');
+    roomNameElement.textContent = room.name;
 
-  const roomDescriptionElement = document.createElement('p');
-  roomDescriptionElement.classList.add('room--description');
-  roomDescriptionElement.textContent = room.description;
+    const roomDescriptionElement = document.createElement('p');
+    roomDescriptionElement.classList.add('room--description');
+    roomDescriptionElement.textContent = room.description;
 
-  const roomPriceElement = document.createElement('p');
-  roomPriceElement.textContent = `Price: ${room.price}`;
+    const roomPriceElement = document.createElement('p');
+    roomPriceElement.textContent = `Price: ${room.price}`;
 
-  const roomGuestsElement = document.createElement('p');
-  roomGuestsElement.textContent = `Guests: ${room.guests}`;
+    const roomGuestsElement = document.createElement('p');
+    roomGuestsElement.textContent = `Guests: ${room.guests}`;
 
-  roomArticle.appendChild(roomNameElement);
-  roomArticle.appendChild(roomDescriptionElement);
-  roomArticle.appendChild(roomPriceElement);
-  roomArticle.appendChild(roomGuestsElement);
+    roomArticle.appendChild(roomNameElement);
+    roomArticle.appendChild(roomDescriptionElement);
+    roomArticle.appendChild(roomPriceElement);
+    roomArticle.appendChild(roomGuestsElement);
 
-  document.body.appendChild(roomArticle);
-}); //forEach end
+    document.body.appendChild(roomArticle);
+  }); //forEach end
+} // end of renderProperties
+
+fetch('./js/properties.json')
+  .then((response) => {
+    return response.json();
+  })
+
+  .then((data) => {
+    //console.log(data);
+    renderProperties(data);
+  });
