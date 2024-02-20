b7f589a8a7b9b10cbebb153f5e32beed8ece6f29
Omar Camara <117489682+Username273183@users.noreply.github.com>
2024-02-20 18:21:48 -0500

    feb20

---
+++ airbnb/js/filter-exercises.js
@@ -0,0 +1,44 @@
+// console.log('Testing');
+
+const numbers = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+];
+
+const filteredNumbers = numbers.filter((number) => {
+  return number > 5 && number < 15;
+});
+
+console.log(filteredNumbers);
+
+// ex 2
+
+const people = [
+  { name: 'John', age: 25 },
+  { name: 'Bill', age: 30 },
+  { name: 'Dave', age: 22 },
+  { name: 'Jill', age: 35 },
+  { name: 'Katie', age: 28 },
+  { name: 'Martha', age: 27 },
+];
+
+const filteredPeople = people.filter((person) => {
+  return person.age < 30;
+});
+
+console.log(filteredPeople);
+
+const cars = [
+  { make: 'Ford', model: 'Fusion', year: 2019 },
+  { make: 'Chevrolet', model: 'Malibu', year: 2018 },
+  { make: 'Ford', model: 'Focus', year: 2017 },
+  { make: 'Chevrolet', model: 'Cruze', year: 2016 },
+  { make: 'Ford', model: 'Taurus', year: 2015 },
+  { make: 'Chevrolet', model: 'Impala', year: 2014 },
+];
+
+const filteredCars = cars.filter((car) => {
+  return car.make === 'Ford';
+}); // end of filter
+
+console.log(filteredCars);
